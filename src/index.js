8cddcdc34b85d069bbc7563a4d18624dd6086a7b
GokulVGirish <gokulvgirish@.com>
2024-05-25 13:54:21 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./incrementDecrement/App";
+//change the folder name in the path to render different components
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
